aa6725aab975092eb66cd37fee69e0e20f659020
phaetton <31552767+phaetton@users.noreply.github.com>
2020-03-18 19:46:15 -0600

    componentes creados
    Componentes creados y en prueba en el html principal

--- TiendaAngularReact/TiendaAngular2/src/app/app.component.ts
+++ TiendaAngularReact/TiendaAngular2/src/app/app.component.ts
@@ -1,5 +1,12 @@
 import { Component } from '@angular/core';
 
+//importamos los componentes y luego colocamos la etiqueta en el html
+import { CarritoComponent } from "./carrito/carrito.component";
+import { CatalogoComponent } from "./catalogo/catalogo.component";
+import { LoginComponent } from "./login/login.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+
+//los decoradores aplican acciones en segundo plano
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
--- TiendaAngularReact/TiendaAngular2/src/app/app.module.ts
+++ TiendaAngularReact/TiendaAngular2/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarritoComponent } from './carrito/carrito.component';
+import { LoginComponent } from './login/login.component';
+import { CatalogoComponent } from './catalogo/catalogo.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarritoComponent,
+    LoginComponent,
+    CatalogoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
